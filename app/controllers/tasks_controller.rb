517555e5683573d1620535bd4e215b7f33d15fb3
naoki.koshiba <innocent-starter-10@docomo.ne.jp>
2021-04-12 14:15:55 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,26 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
+
   def index
+
       @tasks = Task.all
   end
 
   def show
       set_task
+      
   end
 
   def new
-      @task =Task.new
+       @task =Task.new
+      
   end
 
   def create
-      @task = Task.new(task_params)
+      # @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success] = 'task　が投稿されました'
@@ -56,6 +63,13 @@ class TasksController < ApplicationController
   
   def task_params
       params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
